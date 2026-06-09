2b8b576cac9878c51143b5c40bb079b441e431bc
K <139063338+Prayzez@users.noreply.github.com>
2026-06-10 01:18:00 +0900

    Add HelloDeveloper Java project

---
+++ src/HelloDeveloper.java
@@ -0,0 +1,11 @@
+
+public class HelloDeveloper {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, my name is K.");
+		System.out.println("My goal is to become a hireable Java backend developer.");
+		System.out.println("I am studying Java because I want to build reliable business systems.");
+
+	}
+
+}
