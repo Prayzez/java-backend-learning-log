f9b30024782f9cfcc522d93f2d27ac76ff20d43b
K <139063338+Prayzez@users.noreply.github.com>
2026-06-23 22:47:43 +0900

    Add constructor practice to StudyGoal

--- src/StudyGoal.java
+++ src/StudyGoal.java
@@ -2,4 +2,13 @@ public class StudyGoal{
 	String goalName;
 	int targetMonths;
 	double weeklyHours;
+	public StudyGoal(
+			String goalName,
+			int targetMonths,
+			double weeklyHours) {
+		this.goalName = goalName;
+		this.targetMonths = targetMonths;
+		this.weeklyHours = weeklyHours;
+	}
+	
 }
--- src/StudyGoalDemo.java
+++ src/StudyGoalDemo.java
@@ -2,11 +2,13 @@ public class StudyGoalDemo {
 
 	public static void main(String[] args) {
 
-		StudyGoal goal = new StudyGoal();
+		StudyGoal goal =
+				new StudyGoal(
+					"Become a Java backend developer",
+					7,
+					20.5
+				);
 
-		goal.goalName = "Become a Java backend developer";
-		goal.targetMonths = 7;
-		goal.weeklyHours = 20.5;
 
 		System.out.println("Study Goal Summary"); // 学習目標のまとめ
 		System.out.println("Goal Name: " + goal.goalName); // 目標名：
@@ -15,11 +17,12 @@ public class StudyGoalDemo {
 
 		System.out.println();
 
-		StudyGoal goal2 = new StudyGoal();
-
-		goal2.goalName = "Pass JLPT N2";
-		goal2.targetMonths = 6;
-		goal2.weeklyHours = 10;
+		StudyGoal goal2 =
+				new StudyGoal(
+					"Pass JLPT N2",
+					6,
+					10
+				);
 
 		System.out.println("Second Goal Summary"); // 2つ目の学習目標のまとめ
 		System.out.println("Goal Name: " + goal2.goalName); // 目標名：
